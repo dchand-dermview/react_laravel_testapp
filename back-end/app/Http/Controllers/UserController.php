2fd8b22db5e86339a40412ee881a1341e19c2a1e
donncha <chdonncha@gmail.com>
2022-08-01 22:01:16 +0100

    added query string handling for user

--- back-end/app/Http/Controllers/UserController.php
+++ back-end/app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Exception;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use App\Models\User as UserModel;
 use Illuminate\Support\Facades\DB;
@@ -16,11 +17,31 @@ class UserController extends BaseController
 
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+
     /**
+     * @param Request $request
      * @return \Illuminate\Http\JsonResponse|object
      */
-    public function getUsers()
+    public function getUsers(Request $request)
     {
+        if ($request->email) {
+            try {
+                $Users = UserModel::where('email', '=', $request->email)->get();
+                return response()->json($Users)->setStatusCode(200);
+            } catch (Exception $e) {
+                return response()->json($Users)->setStatusCode(500);
+            }
+        }
+
+        if ($request->username) {
+            try {
+                $Users = UserModel::where('username', '=', $request->username)->get();
+                return response()->json($Users)->setStatusCode(200);
+            } catch (Exception $e) {
+                return response()->json($Users)->setStatusCode(500);
+            }
+        }
+
         try {
             $Users = UserModel::get();
             return response()->json($Users)->setStatusCode(200);
@@ -29,8 +50,9 @@ class UserController extends BaseController
         }
     }
 
-    public function getRandomUser() {
-        $randomInt = rand(1,100);
+    public function getRandomUser()
+    {
+        $randomInt = rand(1, 100);
         try {
             $Users = UserModel::where('id', '=', $randomInt)->get();
             return response()->json($Users)->setStatusCode(200);
@@ -39,7 +61,8 @@ class UserController extends BaseController
         }
     }
 
-    public function getUserById($id) {
+    public function getUserById($id)
+    {
         try {
             $Users = UserModel::where('id', '=', $id)->get();
             return response()->json($Users)->setStatusCode(200);
@@ -48,16 +71,10 @@ class UserController extends BaseController
         }
     }
 
-    public function getUserByUsername($username) {
-        try {
-            $Users = UserModel::where('username', '=', $username)->get();
-            return response()->json($Users)->setStatusCode(200);
-        } catch (Exception $e) {
-            return response()->json($Users)->setStatusCode(500);
-        }
-    }
-
-
+//    public function getUserByUsername($username)
+//    {
+//
+//    }
 }
 
 /*
@@ -65,11 +82,12 @@ TODO
 
 post create
 patch update
-get a given page - {{base_url}}/{{users_endpoint}}?page={{page}}&limit={{limit}}
+
+need to add the "?email=data" etc to all the endpoints so it can differenciate
+
 get filtering by email - {{base_url}}/{{users_endpoint}}?email={{email}}
 get filtering by username - {{base_url}}/{{users_endpoint}}?username={{username}}
 get filtering by email and username - {{base_url}}/{{users_endpoint}}?email={{email}}&username={{username}}
-
 
 add user status'
 get by id - {{base_url}}/{{statuses_endpoint}}/{{status_id}}
