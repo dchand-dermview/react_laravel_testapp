2ad6cd934b893c70194ed5aecb1220792bdabd91
chdonncha <chdonncha@gmail.com>
2022-08-14 19:41:26 +0100

    testing 5

--- back-end/database/factories/UserFactory.php
+++ back-end/database/factories/UserFactory.php
@@ -14,7 +14,7 @@ class UserFactory extends Factory
     public function definition()
     {
         $gender = $this->faker->randomElement(['male', 'female',  "other"]);
-        //testing 4
+        //testing 5
 
         $startDate = '1970-00-00 00:00:00';
         $endDate = '2004-01-07 00:00:00';
