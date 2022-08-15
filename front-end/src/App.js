6b5e00495566c17b2569fb51b0ba1cb9c4239594
chdonncha <chdonncha@gmail.com>
2022-08-16 00:58:29 +0100

    base error page and routing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from "./pages/home/index";
+import ErrorPage from "./pages/errorpage/index";
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />}/>
+                <Route path="*" element={<ErrorPage />}/>
             </Routes>
         </Router>
     );
