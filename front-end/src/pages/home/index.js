5f2451cff7e1ef5e0d762460b3bc25c749b4993e
chdonncha <chdonncha@gmail.com>
2022-08-19 23:51:00 +0100

    basic http request output

--- front-end/src/pages/home/index.js
+++ front-end/src/pages/home/index.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+
 function Home() {
-    return <div>THIS IS THE HOME PAGE</div>
+    fetch('https://randomuser.me/api/?results=100')
+        .then(response => response.json())
+        .then(data => this.setState({totalReactPackages: data}));
+
+    return (
+        <div>
+            <h1>THIS IS THE HOME PAGE</h1>
+            <h2>It is {this.state.totalReactPackages}.</h2>
+        </div>
+    );
 }
+
 
 export default Home;
